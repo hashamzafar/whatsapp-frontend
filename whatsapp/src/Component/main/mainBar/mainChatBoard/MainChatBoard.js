db921667719b205385d9b2b32e964a913f0a93bc
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-13 16:19:18 +0200

    socket(connect)

--- whatsapp/src/Component/main/mainBar/mainChatBoard/MainChatBoard.js
+++ whatsapp/src/Component/main/mainBar/mainChatBoard/MainChatBoard.js
@@ -1,20 +1,30 @@
-import React from 'react'
-import { Row } from 'react-bootstrap'
-import './mainchatboard.css'
-import ScrollToBottom from 'react-scroll-to-bottom'
-import Message from './message/Message.js'
+import { useEffect, useState } from "react";
+import { Row } from "react-bootstrap";
+import "./mainchatboard.css";
+import ScrollToBottom from "react-scroll-to-bottom";
+import Message from "./message/Message.js";
+import { io } from "socket.io-client";
 
 function MainChatBoard({ messages }) {
-    return (
-        <Row id="mainchatboard">
-            <ScrollToBottom>
-                {messages && messages.map((message, i) => <Message key={i} message={message} />)}
-                <div id='message'>
-                    hi there
-                </div>
-            </ScrollToBottom>
-        </Row>
-    )
+  const url = `http://localhost:3003`;
+  const socket = io(url, { transports: ["websocket"] });
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connection STARTED");
+      console.log(socket.id);
+      socket.on("welcome");
+    });
+  }, []);
+  return (
+    <Row id="mainchatboard">
+      <ScrollToBottom>
+        {messages &&
+          messages.map((message, i) => <Message key={i} message={message} />)}
+        <div id="message">hi there</div>
+      </ScrollToBottom>
+    </Row>
+  );
 }
 
-export default MainChatBoard
+export default MainChatBoard;
--- whatsapp/src/Component/registration/Registration.jsx
+++ whatsapp/src/Component/registration/Registration.jsx
@@ -1,127 +1,113 @@
-import { Formik, Field, Form } from "formik"
-import { Container } from "react-bootstrap"
-import { Link } from "react-router-dom"
-import "./register.css"
-
+import { Formik, Field, Form } from "formik";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./register.css";
 
 const Registration = () => {
-    const onSubmit = (values, actions) => {
-        console.log(values)
-        createUser(values)
+  const onSubmit = (values, actions) => {
+    console.log(values);
+    createUser(values);
+  };
+
+  const createUser = async (values) => {
+    console.log("inside create user");
+
+    try {
+      let response = await fetch("", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+      if (response.ok) {
+        alert("USER SAVED!");
+        let dataRequested = await response.json();
+        console.log(dataRequested);
+        window.localStorage.setItem("user_Token", dataRequested.access_token);
+      } else {
+        alert("User not created");
+      }
+    } catch (e) {
+      return e;
     }
+  };
 
-    const createUser = async (values) => {
-        console.log("inside create user")
+  return (
+    <>
+      <Formik
+        onSubmit={onSubmit}
+        initialValues={{
+          name: "",
+          email: "",
+          url: "",
+          password: "",
+        }}
+      >
+        {(props) => (
+          <Container className="col-md-8">
+            <img
+              src="https://image.similarpng.com/very-thumbnail/2020/07/Whatsapp-icon-vector-PNG.png"
+              width="300"
+              height="300"
+              alt=""
+            />
+            <h3 id="title">Join to Whatsapp</h3>
+            <Form>
+              <div className="form-group">
+                <label htmlFor="name">Full Name :</label>
+                <Field
+                  id="name"
+                  className="form-control"
+                  name="name"
+                  type="text"
+                />
+              </div>
 
-        try {
-            let response = await fetch(
-                "",
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(values),
-                }
-            )
-            if (response.ok) {
-                alert("USER SAVED!")
-                let dataRequested = await response.json()
-                console.log(dataRequested)
-                window.localStorage.setItem("user_Token", dataRequested.access_token)
-            } else {
-                alert("User not created")
-            }
-        } catch (e) {
-            return e
-        }
-    }
+              <div className="form-group">
+                <label htmlFor="email"> Email Address :</label>
+                <Field
+                  id="email"
+                  className="form-control"
+                  name="email"
+                  type="email"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password"> Password :</label>
+                <Field
+                  id="password"
+                  className="form-control"
+                  name="password"
+                  type="password"
+                />
+              </div>
 
-    return (
-        <>
+              <div className="form-group">
+                <label htmlFor="username">Picture url :</label>
+                <Field
+                  id="url"
+                  className="form-control"
+                  name="url"
+                  type="url"
+                />
+              </div>
 
-            <Formik
-                onSubmit={onSubmit}
-                initialValues={{
-                    name: "",
-                    email: "",
-                    url: "",
-                    password: "",
-
-                }}
-            >
-                {(props) => (
-                    <Container className="col-md-8">
-                        <img src="https://image.similarpng.com/very-thumbnail/2020/07/Whatsapp-icon-vector-PNG.png" width="300" height="300" alt="" />
-                        <h3 id="title">Join to Whatsapp</h3>
-                        <Form>
-                            <div className="form-group">
-                                <label htmlFor="name">
-
-                                    Full Name :
-                                </label>
-                                <Field
-                                    id="name"
-                                    className="form-control"
-                                    name="name"
-                                    type="text"
-                                />
-                            </div>
-
-                            <div className="form-group">
-                                <label htmlFor="email">
-                                    {" "}
-
-                                    Email Address :
-                                </label>
-                                <Field
-                                    id="email"
-                                    className="form-control"
-                                    name="email"
-                                    type="email"
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="password">
-                                    {" "}
-
-                                    Password :
-                                </label>
-                                <Field
-                                    id="password"
-                                    className="form-control"
-                                    name="password"
-                                    type="password"
-                                />
-                            </div>
-
-
-                            <div className="form-group">
-                                <label htmlFor="username">
-
-                                    Picture url :
-                                </label>
-                                <Field
-                                    id="url"
-                                    className="form-control"
-                                    name="url"
-                                    type="url"
-                                />
-                            </div>
-
-                            <button
-                                id="btn"
-                                type="submit"
-                                className="btn btn-success my-2 btn-large w-100"
-                            >
-                                <Link to="/" id="link">      Register</Link>
-                            </button>
-
-                        </Form>
-                    </Container>
-                )}
-            </Formik>
-        </>
-    )
-}
-export default Registration
+              <button
+                id="btn"
+                type="submit"
+                className="btn btn-success my-2 btn-large w-100"
+              >
+                <Link to="/" id="link">
+                  {" "}
+                  Register
+                </Link>
+              </button>
+            </Form>
+          </Container>
+        )}
+      </Formik>
+    </>
+  );
+};
+export default Registration;
