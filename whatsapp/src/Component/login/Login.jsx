a73b355beafbfa1bc7e05bca71f7147e537d9cf5
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-14 10:35:52 +0200

    login done

--- whatsapp/src/Component/login/Login.jsx
+++ whatsapp/src/Component/login/Login.jsx
@@ -1,77 +1,135 @@
-import { Form, Button, Container } from 'react-bootstrap'
-import { FaFacebook } from "react-icons/fa"
-import { FcGoogle } from "react-icons/fc"
-import "./login.css"
-import { Link } from "react-router-dom"
-import { Formik, Field } from "formik"
+import { Form, Button, Container } from "react-bootstrap";
+import { FaFacebook } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import "./login.css";
+import { Link, withRouter } from "react-router-dom";
+import { Formik, Field } from "formik";
+import { useEffect, useState } from "react";
 
-const Login = () => (
+const Login = (props) => {
+  const [login, setLogin] = useState({
+    email: "",
+    password: "",
+  });
+  //   console.log("login props=", props);
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setLogin((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
 
-    <Formik
+  //   useEffect(()=>{
+  const submitLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `https://what-s-app.herokuapp.com/users/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(login),
+        }
+      );
+      if (response.ok) {
+        alert("LOGIN!!!!");
+        props.history.push("/main");
+        const res = await response.json();
+        window.localStorage.setItem("Token", res.accessToken);
+        console.log(res);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //   },[])
 
-        initialValues={{
-            email: "",
-            password: "",
+  return (
+    <Container>
+      <Form id="login" onSubmit={submitLogin}>
+        <img
+          src="https://image.similarpng.com/very-thumbnail/2020/07/Whatsapp-icon-vector-PNG.png"
+          width="300"
+          height="300"
+          alt=""
+        />
+        <h3 id="title">Welcome to Whatsapp</h3>
 
-        }}
-    >
-        <Container>
-            <Form id="login">
-                <img src="https://image.similarpng.com/very-thumbnail/2020/07/Whatsapp-icon-vector-PNG.png" width="300" height="300" alt="" />
-                <h3 id="title">Welcome to Whatsapp</h3>
+        <div className="form-group">
+          <label htmlFor="email"> Email Address :</label>
+          <input
+            id="email"
+            className="form-control"
+            name="email"
+            type="email"
+            value={login.email}
+            onChange={handleChange}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </div>
+        <div className="form-group">
+          <label>Password :</label>
+          <input
+            id="password"
+            className="form-control"
+            name="password"
+            type="password"
+            value={login.password}
+            onChange={handleChange}
+          />
+        </div>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Remember me" />
+        </Form.Group>
+        <Button
+          variant="success"
+          type="submit"
+          className="my-2 btn btn-large w-100"
+          id="btn"
+        >
+          {/* <Link to="/main" className="link">
+            {" "} */}
+          Log in
+          {/* </Link> */}
+        </Button>
+        <a href="http://google.com" target="_blank">
+          <Button
+            variant="light"
+            type="submit"
+            className="my-2 btn btn-large w-100 border"
+            id="btn"
+          >
+            <FcGoogle className="bg-light" /> Log in with google
+          </Button>
+        </a>
+        <Button
+          variant="primary"
+          type="submit"
+          className="my-2 btn btn-large w-100"
+          id="btn"
+        >
+          <FaFacebook className="bg-primary" /> Log in with facebook
+        </Button>
 
-                <div className="form-group">
-                    <label htmlFor="email">
-                        {" "}
+        <Button
+          variant="primary"
+          type="submit"
+          className="my-2 btn-large w-100 btn"
+          id="btn"
+        >
+          <Link to="/register" className="link">
+            {" "}
+            Register
+          </Link>
+        </Button>
+      </Form>
+    </Container>
+  );
+};
 
-                        Email Address :
-                    </label>
-                    <Field
-                        id="email"
-                        className="form-control"
-                        name="email"
-                        type="email"
-                    />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </div>
-                <div className="form-group">
-                    <label >
-                        Password :
-                    </label>
-                    <Field
-                        id="password"
-                        className="form-control"
-                        name="password"
-                        type="password"
-                    />
-                </div>
-                <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Remember me" />
-                </Form.Group>
-                <Button variant="success" type="submit" className="my-2 btn btn-large w-100" id="btn">
-                    <Link to="/main" className="link">      Log in</Link>
-                </Button>
-                <a href="http://google.com" target="_blank" >
-                    <Button variant="light" type="submit" className="my-2 btn btn-large w-100 border" id="btn">
-                        <FcGoogle className="bg-light" /> Log in with google
-                    </Button></a>
-                <Button variant="primary" type="submit" className="my-2 btn btn-large w-100" id="btn" >
-                    <FaFacebook className="bg-primary" /> Log in with facebook
-                </Button>
-
-                <Button variant="primary" type="submit" className="my-2 btn-large w-100 btn" id="btn" >
-                    <Link to="/register" className="link">         Register</Link>
-                </Button>
-
-            </Form>
-        </Container>
-    </Formik >
-
-
-)
-
-
-
-export default Login
+export default withRouter(Login);
