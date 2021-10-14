36c5996489c06bc268ca1f2fd1fbe511ab2ce09a
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-14 12:15:00 +0200

    //single contact

--- whatsapp/src/Component/main/sideBar/contactBoard/singleContact/SingleContact.js
+++ whatsapp/src/Component/main/sideBar/contactBoard/singleContact/SingleContact.js
@@ -1,22 +1,34 @@
-import React from 'react'
-import './singlecontact.css'
-import { Row } from 'react-bootstrap'
+import React, { useState } from "react";
+import "./singlecontact.css";
+import { Row } from "react-bootstrap";
 
 function SingleContact() {
-    return (
-        <Row id="singleContact">
-            <img src='https://picsum.photos/200' alt='profileImage' />
-            <div className='details'>
-                <div>
-                    <span>Contact Name</span>
-                    <span>time</span>
-                </div>
-                <div>
-                    <p>last message</p>
-                </div>
-            </div>
-        </Row>
-    )
+  // const [contact, steContact]= useState([])
+  // const fetchUser = async()=>{
+  //     try {
+  //         const response = await fetch(`https://what-s-app.herokuapp.com/users`, {
+  //             headers: {
+
+  //             }
+  //         })
+  //     } catch (error) {
+  //         console.log(error)
+  //     }
+  // }
+  return (
+    <Row id="singleContact">
+      <img src="https://picsum.photos/200" alt="profileImage" />
+      <div className="details">
+        <div>
+          <span>Contact Name</span>
+          <span>time</span>
+        </div>
+        <div>
+          <p>last message</p>
+        </div>
+      </div>
+    </Row>
+  );
 }
 
-export default SingleContact
+export default SingleContact;
